b9b450b1ad1c54af0ff42ac293f8857006bc1364
Youssef-Harby <youssef_harby@yahoo.com>
2023-09-10 16:46:17 +0300

    pydantic: config keys have changed in V2

--- 01-task-microservices/app/schemas.py
+++ 01-task-microservices/app/schemas.py
@@ -13,8 +13,8 @@ class BookBaseSchema(BaseModel):
     updatedAt: datetime = Field(default_factory=datetime.utcnow)
 
     class Config:
-        orm_mode = True
-        allow_population_by_field_name = True
+        from_attributes = True
+        populate_by_name = True
         arbitrary_types_allowed = True
 
 
